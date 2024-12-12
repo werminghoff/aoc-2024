3171c8b542ee328e92ac6d2f1fc15b700c8e89e1
Bruno Werminghoff <brunowerminghoff@gmail.com>
2024-12-12 17:07:11 +0100

    Updated day 11 part 1 with performance improvements, just for fun

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -9,59 +9,111 @@ fn main() {
 
 mod part1 {
 
+    use std::collections::HashMap;
+
+    #[inline]
+    fn number_of_digits(value: usize) -> usize {
+        (value.ilog10() + 1) as usize
+    }
+
+    #[inline]
     fn is_double_digit(value: usize) -> bool {
-        format!("{value}").len() % 2 == 0
+        //format!("{value}").len() % 2 == 0
+        number_of_digits(value) % 2 == 0
     }
 
+    #[inline]
     fn split_digits(value: usize) -> (usize, usize) {        
-        let fmt = format!("{value}");
-        let res = fmt.split_at(fmt.len() / 2);
-        (res.0.parse().unwrap(), res.1.parse().unwrap())
+        let digits = number_of_digits(value);
+
+        let divisor = (10 as usize).pow((digits/2) as u32);
+
+        let left_side = value / divisor;
+        let right_side = value - (left_side * divisor);
+
+        (left_side, right_side)
     }
 
-    fn blink(input: &mut Vec<usize>) {
+    fn add_cache(cache: &mut HashMap<BlinkerCache, usize>, input: usize, depth: usize, result: usize) {
+        let val = BlinkerCache {
+            input: input,
+            depth: depth
+        };
+        _ = cache.insert(val, result);
+    }
 
-        let mut idx = 0;
-        loop {
-            if idx >= input.len() {
-                break
-            }
-
-            let val = input[idx];
-
-            if val == 0 {
-                input[idx] = 1
-            } else if is_double_digit(val) {
-                let new_vals = split_digits(val);
-                input[idx] = new_vals.1;
-                input.insert(idx, new_vals.0);
-                idx +=1;
-            } else {
-                input[idx] = val * 2024;
-            }
-
-            idx += 1;
+    fn get_cached(cache: &HashMap<BlinkerCache, usize>, input: usize, depth: usize) -> Option<usize> {
+        let val = BlinkerCache {
+            input: input,
+            depth: depth
+        };
+        match cache.get(&val) {
+            Some(v) => Some(*v),
+            None => None
         }
-
     }
 
-    fn blink_n_times(input: &mut Vec<usize>, times: usize) {
+
+    #[inline]
+    fn blink_stone_counter(input: usize, cache: &mut HashMap<BlinkerCache, usize>, depth: usize) -> usize {
+        let mut wrapper = || {
+            if depth == 0 {
+                return 1;
+            }
+
+            if let Some(cached_val) = get_cached(cache, input, depth) {
+                return cached_val;
+            }
+
+            if input == 0 {
+                return blink_stone_counter(1, cache, depth - 1)
+            } else if is_double_digit(input) {
+                let new_vals = split_digits(input);
+                let left = blink_stone_counter(new_vals.1, cache, depth - 1);
+                let right = blink_stone_counter(new_vals.0, cache, depth - 1);
+                return left + right;
+            } else {
+                return blink_stone_counter(input * 2024, cache, depth - 1);
+            }
+        };
+
+        let res = wrapper();
+        add_cache(cache, input, depth, res);
+        res
+    }
+
+    fn blink_n_times(input: &Vec<usize>, times: usize) {
         if times < 1 {
             return
         }
-        for _ in 0..times {
-            blink(input);
+
+        let mut cache = HashMap::<BlinkerCache, usize>::new();
+
+        let collected: Vec<usize> = input.iter().map(|it| {
+            let res = blink_stone_counter(*it, &mut cache, times);
+            return res
+        }).collect();
+
+        let mut sum = 0;
+        for f in collected {
+            sum += f;
         }
+
+        println!("Part 1 result: {sum}");
+    }
+
+    #[derive(Debug, PartialEq, Eq, Hash)]
+    struct BlinkerCache {
+        pub input: usize,
+        pub depth: usize
     }
 
     pub fn run(contents: &str) {
-        let mut input: Vec<usize> = contents.split_whitespace()
+        let input: Vec<usize> = contents.split_whitespace()
                                         .map(|it| it.parse().unwrap())
                                         .collect();
 
-        blink_n_times(&mut input, 25);
-        let stones = input.len();
-        println!("Part 1 result: {stones}");
+        blink_n_times(&input, 25);
     }
 
 }
