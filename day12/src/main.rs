d525cc41816a06e5705b8efbbe1c6a0e6cb6b27c
Bruno Rigo Werminghoff <brunowerminghoff@gmail.com>
2024-12-13 12:53:59 +0100

    Removed useless code

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -11,55 +11,6 @@ fn main() {
 
 type Matrix = Vec<Vec<char>>;
 
-trait BorderBitFlag {
-    fn border_bitflag(top: bool, bottom: bool, left: bool, right: bool) -> Self;
-    fn has_top_border(&self) -> bool;
-    fn has_bottom_border(&self) -> bool;
-    fn has_left_border(&self) -> bool;
-    fn has_right_border(&self) -> bool;
-}
-
-const BIT_FLAG_TOP: u8      = 0b0001;
-const BIT_FLAG_BOTTOM: u8   = 0b0010;
-const BIT_FLAG_LEFT: u8     = 0b0100;
-const BIT_FLAG_RIGHT: u8    = 0b1000;
-
-impl BorderBitFlag for u8 {
-
-    fn border_bitflag(top: bool, bottom: bool, left: bool, right: bool) -> Self {
-        let mut res = 0;
-        if top {
-            res = res | BIT_FLAG_TOP;
-        }
-        if bottom {
-            res = res | BIT_FLAG_BOTTOM;
-        }
-        if left {
-            res = res | BIT_FLAG_LEFT;
-        }
-        if right {
-            res = res | BIT_FLAG_RIGHT;
-        }
-        res
-    }
-    
-    fn has_top_border(&self) -> bool {
-        (self & BIT_FLAG_TOP) == BIT_FLAG_TOP
-    }
-    
-    fn has_bottom_border(&self) -> bool {
-        (self & BIT_FLAG_BOTTOM) == BIT_FLAG_BOTTOM
-    }
-    
-    fn has_left_border(&self) -> bool {
-        (self & BIT_FLAG_LEFT) == BIT_FLAG_LEFT
-    }
-
-    fn has_right_border(&self) -> bool {
-        (self & BIT_FLAG_RIGHT) == BIT_FLAG_RIGHT
-    }
-}
-
 fn parse(contents: &str) -> Matrix {
     contents.split_whitespace()
                                         .map(|it| { 
@@ -114,31 +65,53 @@ struct Cache<'a> {
 impl Cache<'_> {
 
     fn has_left_border_at(&self, position: &Position) -> bool {
-        let Some(val) = self.borders.get(&position) else {
-            return false;
-        };
-        val.has_left_border()
+        if position.x < 0 {
+            false
+        } else if position.x == 0 {
+            true
+        } else if self.matrix[position.y as usize][position.x as usize] != self.matrix[position.y as usize][position.x as usize -1] {
+            true
+        } else {
+            false
+        }
     }
 
     fn has_right_border_at(&self, position: &Position) -> bool {
-        let Some(val) = self.borders.get(&position) else {
-            return false;
-        };
-        val.has_right_border()
+        let limit = self.matrix[position.y as usize].len() - 1;
+        if position.x as usize > limit {
+            false
+        } else if position.x as usize == limit {
+            true
+        } else if self.matrix[position.y as usize][position.x as usize] != self.matrix[position.y as usize][position.x as usize + 1] {
+            true
+        } else {
+            false
+        }
     }
 
     fn has_top_border_at(&self, position: &Position) -> bool {
-        let Some(val) = self.borders.get(&position) else {
-            return false;
-        };
-        val.has_top_border()
+        if position.y < 0 {
+            false
+        } else if position.y == 0 {
+            true
+        } else if self.matrix[position.y as usize][position.x as usize] != self.matrix[position.y  as usize -1][position.x as usize] {
+            true
+        } else {
+            false
+        }
     }
 
     fn has_bottom_border_at(&self, position: &Position) -> bool {
-        let Some(val) = self.borders.get(&position) else {
-            return false;
-        };
-        val.has_bottom_border()
+        let limit = self.matrix.len() - 1;
+        if position.y as usize > limit {
+            false
+        } else if position.y as usize == limit {
+            true
+        } else if self.matrix[position.y as usize][position.x as usize] != self.matrix[position.y as usize + 1][position.x as usize] {
+            true
+        } else {
+            false
+        }
     }
 
 }
@@ -281,11 +254,9 @@ fn print_debug_plot(cache: &Cache, plot: &Plot) {
 
 mod part1 {
     
-    use super::Matrix;
     use crate::Cache;
     use super::Position;
     use super::Plot;
-    use super::BorderBitFlag;
     use std::collections::{HashMap, HashSet};
 
     pub fn run(contents: &str) {
@@ -295,27 +266,7 @@ mod part1 {
             borders: HashMap::new()
         };
 
-        create_border_cache(&mut cache);
         let plots = find_plots(&mut cache);
-
-    }
-
-    fn create_border_cache(data: &mut Cache) {
-        for (y, row) in data.matrix.iter().enumerate() {
-            for (x, _) in row.iter().enumerate() {
-
-                let position = Position::new(x as i64, y as i64);
-
-                let left_border = has_left_border(&position, &data.matrix);
-                let right_border = has_right_border(&position, &data.matrix);
-                let top_border = has_top_border(&position, &data.matrix);
-                let bottom_border = has_bottom_border(&position, &data.matrix);
-
-                let val = u8::border_bitflag(top_border, bottom_border, left_border, right_border);
-                _ = data.borders.insert(position, val);
-
-            }
-        }
     }
 
     fn find_plots(cache: &mut Cache) -> Vec<Plot> {
@@ -368,56 +319,6 @@ mod part1 {
         find_plot_visiting(cache, visited, current_plot, position.to_right());
         find_plot_visiting(cache, visited, current_plot, position.to_top());
         find_plot_visiting(cache, visited, current_plot, position.to_bottom());
-    }
-
-    fn has_top_border(position: &Position, matrix: &Matrix) -> bool {
-        if position.y < 0 {
-            false
-        } else if position.y == 0 {
-            true
-        } else if matrix[position.y as usize][position.x as usize] != matrix[position.y  as usize -1][position.x as usize] {
-            true
-        } else {
-            false
-        }
-    }
-
-    fn has_bottom_border(position: &Position, matrix: &Matrix) -> bool {
-        let limit = matrix.len() - 1;
-        if position.y as usize > limit {
-            false
-        } else if position.y as usize == limit {
-            true
-        } else if matrix[position.y as usize][position.x as usize] != matrix[position.y as usize + 1][position.x as usize] {
-            true
-        } else {
-            false
-        }
-    }
-
-    fn has_left_border(position: &Position, matrix: &Matrix) -> bool {
-        if position.x < 0 {
-            false
-        } else if position.x == 0 {
-            true
-        } else if matrix[position.y as usize][position.x as usize] != matrix[position.y as usize][position.x as usize -1] {
-            true
-        } else {
-            false
-        }
-    }
-    
-    fn has_right_border(position: &Position, matrix: &Matrix) -> bool {
-        let limit = matrix[position.y as usize].len() - 1;
-        if position.x as usize > limit {
-            false
-        } else if position.x as usize == limit {
-            true
-        } else if matrix[position.y as usize][position.x as usize] != matrix[position.y as usize][position.x as usize + 1] {
-            true
-        } else {
-            false
-        }
     }
 
 }
